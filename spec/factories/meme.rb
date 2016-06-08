d546d04e87e6bd322035fca40a111962944f804d
Amy Nicholson <missamynicholson@gmail.com>
2016-06-08 22:10:40 +0100

    Modify view and test to allow for Meme model inclusion

--- spec/factories/meme.rb
+++ spec/factories/meme.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     top_caption "When you realise Adam is on master branch"
     bottom_caption "and he's trying to push to github"
     raw_image_url "master-branch.jpg"
-    # image_url Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/master-branch.jpg', 'image/jpg')
+    image Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/master-branch.jpg', 'image/jpg')
+    id 1
   end
 end
--- spec/features/viewing_memes_spec.rb
+++ spec/features/viewing_memes_spec.rb
@@ -1,14 +1,19 @@
 feature "viewing the memes" do
   before do
     meme = create :meme
+    meme2 = create :meme, top_caption: "Top2", id: 2
   end
-  context "can see memes" do
-    scenario "shows a meme on the homepage" do
-      visit '/'
-      expect(page).to have_content("When you realise Adam is on master branch")
-      expect(page).to have_content("and he's trying to push to github")
-      expect(page).to have_content("master-branch.jpg")
-      # expect(page).to have_css("img[src*='master-branch.jpg']")
-    end
+
+  scenario "shows a meme on the homepage" do
+    visit '/'
+    expect(page).to have_content("When you realise Adam is on master branch")
+    expect(page).to have_content("and he's trying to push to github")
+    expect(page).to have_css("img[src*='master-branch.jpg']")
+  end
+
+  scenario "shows multiple memes on the homepage" do
+    visit '/'
+    expect(page).to have_content("When you realise Adam is on master branch")
+    expect(page).to have_content("Top2")
   end
 end
