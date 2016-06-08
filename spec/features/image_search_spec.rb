1667f76f6d17018481b2725a4af6617ba037e5ce
ollieh-m <ollie.hm@cantab.net>
2016-06-08 10:58:25 +0100

    clicking image and creating meme-ish

--- spec/features/image_search_spec.rb
+++ spec/features/image_search_spec.rb
@@ -2,21 +2,30 @@ require 'rails_helper'
 
 feature 'image search' do
 
-	scenario 'user can search for images and see the results on the page' do
+	scenario 'logged in user can search for images and see the results on the page' do
+		sign_up
 		search
 		expect(page).to have_css("img[src*='testimageuri']")
 	end
 
-	scenario 'user can search using more than one word' do
+	scenario 'logged in user can search using more than one word' do
+		sign_up
 		search(searchterm: 'Angry dog')
 		expect(page).to have_css("img[src*='testimageuri']")
 	end
 
-  scenario 'user can select an image' do
+  scenario 'logged in user can select an image' do
+    sign_up
     search
-    click_link('1')
-    expect(page).to have_content("Image added to meme.")
+    click_link('0')
+    expect(page).to have_content("Image added to meme")
     expect(Meme.all.count).to equal(1)
+  end
+
+  scenario 'non-logged in user cannot make a meme' do
+    visit('/')
+		click_link('Make meme')
+    expect(current_path).to eq(new_user_session_path)
   end
 
 end
